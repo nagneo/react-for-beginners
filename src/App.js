08cecfdb5d7bc38e2e3d94869fc070884091ff1a
SaenaKIM <tosk3927@gmail.com>
2022-07-03 19:26:14 +0900

    [TIL] React - refresh component - useEffect, dependency list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {useState, useEffect} from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter])
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when keyword or counter changes");
+  }, [counter, keyword]);
   return (
     <div >
+      <input value={keyword}
+             onChange={onChange}
+             type="text"
+             placeholder="Search here.."/>
       <h1>
-        Welcome Back!
+        {counter}
       </h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
