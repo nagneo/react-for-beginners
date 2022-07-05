a6217652dd93efbd93d49c385b3deb4c736413f1
SaenaKIM <tosk3927@gmail.com>
2022-07-05 18:37:53 +0900

    [TIL] Movie App part one - get api for now playing movies

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
 import {useState, useEffect} from "react";
 
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json)
-        setLoading(false)
-      })
-  });
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch (
+        API_URL + "movie/now_playing?api_key=c405dd59d0f6c3a33613ea5f3006fb27&language=en-US&page=1"
+      )
+    ).json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+  const getGenres = async () => {
+    const json = await (
+      await fetch (
+        API_URL + "genre/movie/list?api_key=c405dd59d0f6c3a33613ea5f3006fb27&language=en-US&page=1&language=en-US"
+      )
+    ).json();
+    setGenres(json.genres);
+    setLoading(false);
+  }
+  useEffect(() =>{
+    getMovies();
+    getGenres();
+  },[]);
   return (
     <div>
-      <h1>
-        The Coins! {loading ? "" : `(${coins.length})`}
-      </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-        ) : (
-        <select>
-        {coins.map((coin) => (
-          <option key={coin.id}>
-            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-          </option>)
-          )
-        }
-        </select>
-        )
-      }
+       {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+      <div>
+        {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={IMAGE_BASE_URL+ "w200" + movie.poster_path} />
+              <h2>{movie.title}</h2>
+              <p>{movie.overview}</p>
+              <ul>
+                {movie.genre_ids.map((gId) => (
+                  <li key={gId}>{genres.find(g => g.id === gId).name}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
